0040080d9df59ca6afdd2bffe7dbb6679bcff3ad
Soumya Suryan <soumyasuryan86@gmail.com>
2026-01-18 18:48:50 +0530

    home page modified

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="  dark:bg-black min-h-screen">
      <Navbar></Navbar>
-     <div className="flex flex-col items-center justify-center pt-40 mx-2">
+     <div className="flex flex-col items-center justify-center pt-20 mx-2">
       <h1 className=" text-3xl sm:text-[50px] ">Your <span className="text-gray-400">Quotation</span> generator</h1>
       <h6 className="text-gray-300 text-sm sm:text-xl">Get your quotation generated with seconds. </h6>
       <a href="/quotation-maker" className="text-black bg-white p-3 mt-10 mb-10 mb-5 text-xl px-5 rounded-lg hover:bg-white/10 hover:text-white hover:border-white border-2">Get Started</a>
